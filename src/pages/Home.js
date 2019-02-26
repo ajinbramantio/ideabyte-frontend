58efd4c58c49f7f7c80e57c18764eab6d3748545
M Haidar Hanif <git@mhaidarhanif.com>
2019-02-26 15:10:22 +0700

    Create Content & Footer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
 import Header from '../components/Header'
+import Content from '../components/Content'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <Content />
+      <Footer />
     </div>
   )
 }
