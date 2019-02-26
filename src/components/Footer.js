28f62c50d8ef960c249b3aaebdace3fb73932e2d
M Haidar Hanif <git@mhaidarhanif.com>
2019-02-26 15:39:00 +0700

    Use styled-components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const StyledFooter = styled.footer`
+  text-align: center;
+  background: #333;
+  color: #fff;
+  padding: 10px 0;
+`
 
 const Footer = () => {
   return (
-    <footer>
+    <StyledFooter>
       <small>Copyright 2019 Idea Byte</small>
-    </footer>
+    </StyledFooter>
   )
 }
 
